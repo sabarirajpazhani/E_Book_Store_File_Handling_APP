d5dd7dbfd706bb709b36c80014630bfd2e06134d
sabarirajp <sabarirajpazhani@gmail.com>
2025-05-12 18:06:27 +0530

    Add project files.

---
+++ E_Book_Store_File_Handling_APP/Program.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace E_Book_Store_File_Handling_APP
+{
+    public class IsNullExpection : Exception
+    {
+        public IsNullExpection(string message) : base(message) { }
+    }
+
+    public class IsValidStringExpection : Exception
+    {
+        public IsValidStringExpection(string message) : base(message) { }
+    }
+    public class Program
+    {
+        public static void isNullString(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new IsNullExpection("Name cannot be Empty");
+            }
+        }
+
+        public static void isValidString(string name)
+        {
+            if (!Regex.IsMatch(name, @"^[a-zA-Z .]+$"))
+            {
+                throw new IsValidStringExpection("Name must contains alphabets only not number and special characters");
+            }
+        }
+
+        public static int BookId = 105;
+
+        //static string bookContentFolder = "Description_File";
+
+        public static string ContentFileDirectory = @"D:\FileHandling\E_Book_App\Description_File";
+
+        static void Main(string[] args)
+        {
+            string FileDirectory = @"D:\FileHandling\E_Book_App";
+            string FilePath = Path.Combine(FileDirectory, "Book_Details.txt");
+
+            while (true)
+            {
+                Console.WriteLine();    
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("----------------------------------------- E - Book Store -----------------------------------------");
+                Console.ResetColor();
+                Console.WriteLine();
+                Console.WriteLine("                                        1. Add the Books                                          ");
+                Console.WriteLine("                                        2. View All Books                                         ");
+                Console.WriteLine("                                        3. View Particular Book Detials                           ");
+                Console.WriteLine("                                        4. Search the Book by Author                              ");
+                Console.WriteLine("                                        5. Update the Book                                        ");
+                Console.WriteLine("                                        6. Delete the Book                                        ");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("--------------------------------------------------------------------------------------------------");
+                Console.ResetColor();
+                Console.WriteLine();
+
+                int Choice = 0;
+                try
+                {
+                    Choice:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Enter the Choice : ");
+                    Console.ResetColor();
+                    int choice = int.Parse(Console.ReadLine());
+
+                    if(choice  == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid choice! Choice cannot be zero. Please re-enter the choice.");
+                        Console.ResetColor();
+                        goto Choice;
+                    }
+                    if(choice > 6)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid choice! Please enter a number between 1 and 7");
+                        Console.ResetColor();
+                        goto Choice;
+                    }
+
+                    Choice = choice;
+                    Console.WriteLine();
+                }
+                catch(FormatException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid choice! Please enter the choice using digits only—no characters, symbols, or whitespace.");
+                    Console.ResetColor();
+                }
+                catch(OverflowException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("nvalid choice! Please enter the correct choice as shown in the header");
+                    Console.ResetColor();
+                }
+
+                String BookName = "None";
+                String BookAuthor = "None";
+                String Genre = "None";
+                String Stock = "None";
+                switch (Choice)
+                {
+                    case 1:
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("                                You Enter '1' for Adding the New Books                            ");
+                        Console.ResetColor();
+                        Console.WriteLine();
+
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("--------------------------------------------------------------------------------------------------");
+                        Console.ResetColor();
+                        Console.WriteLine();
+
+                        BookName:
+                        try
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("Enter the Book Name    : ");
+                            Console.ResetColor();
+                            string bookName = Console.ReadLine();
+
+                            isNullString(BookName);
+                            isValidString(BookName);
+
+                            BookName = bookName;    
+
+                        }
+                        catch (IsNullExpection e)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(e.Message);
+                            Console.ResetColor();
+                            goto BookName;
+                        }
+                        catch(IsValidStringExpection e)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red; 
+                            Console.WriteLine(e.Message);   
+                            Console.ResetColor();   
+                            goto BookName;
+                        }
+
+                        BookAuthor:
+                        try
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("Enter the Author of the Book : ");
+                            Console.ResetColor();
+                            String bookAuthor = Console.ReadLine();
+
+                            isNullString(BookAuthor);
+                            isValidString(BookAuthor);
+
+                            BookAuthor = bookAuthor;    
+                        }
+                        catch (IsNullExpection e)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(e.Message);
+                            Console.ResetColor();
+                            goto BookAuthor;
+                        }
+                        catch (IsValidStringExpection e)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(e.Message);
+                            Console.ResetColor();
+                            goto BookAuthor;
+                        }
+
+                        Genre:
+                        try
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("Enter the Genre of Book : ");
+                            Console.ResetColor();
+                            String genre = Console.ReadLine();
+                            isNullString(genre);
+                            isValidString(genre);
+
+                            Genre = genre;
+                        }
+                        catch (IsNullExpection e)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(e.Message);
+                            Console.ResetColor();
+                            goto Genre;
+                        }
+                        catch (IsValidStringExpection e)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(e.Message);
+                            Console.ResetColor();
+                            goto Genre;
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write($"Enter the Stock of {BookName} : ");
+                        Console.ResetColor();
+                        Stock = Console.ReadLine();
+
+                        BookId++;
+
+                        string contentFileName = $"{BookId}.txt";
+                        string contentFilePath = Path.Combine(ContentFileDirectory, contentFileName);
+
+                        if (!Directory.Exists(ContentFileDirectory))
+                        {
+                            Directory.CreateDirectory(ContentFileDirectory);
+                        }
+
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("----------------------- Enter the Book Descrption -----------------------");
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        string desciption = Console.ReadLine(); 
+
+                        File.WriteAllText(contentFilePath, desciption);
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("-------------------------------------------------------------------------");
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"{contentFilePath} has been created with Description");
+                        Console.ResetColor();
+                        Console.WriteLine();
+                        
+
+                        string BookDetails = $"{BookId},{BookName},{BookAuthor},{Genre},{contentFileName},{Stock}";
+
+                        File.AppendAllText(FilePath, BookDetails+Environment.NewLine);
+
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("New Book has been added Successfully");
+                        Console.ResetColor();
+                        Console.WriteLine();
+
+                        break;
+
+
+
+                }
+            }
+
+
+            
+
+        }
+    }
+}
